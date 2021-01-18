ffc59bc8bf64e4baf9550ed67820c524fdb6377b
Mederbek <kanatbekov.meder.00@gmail.com>
2021-01-18 19:57:08 +0600

    created an app and added login and password auth

---
+++ app/src/main/java/kmn/com/AuthActivity.java
@@ -0,0 +1,69 @@
+package kmn.com;
+
+import androidx.annotation.StringRes;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class AuthActivity extends AppCompatActivity {
+    private Button mEnter;
+    private Button mRegister;
+    private EditText mLogin;
+    private EditText mPassword;
+
+    private View.OnClickListener mOnRegisterClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            // todo Обработка нажатия по этой кнопке
+        }
+    };
+
+    private View.OnClickListener mOnEnterClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            // todo Обработка нажатия по этой кнопке
+            if (isEmailValid() && isPasswordValid()) {
+                // переход к приложению
+            } else {
+                showMessage(R.string.login_input_error);
+            }
+        }
+    };
+
+    private boolean isEmailValid() {
+        return TextUtils.isEmpty(mLogin.getText())
+                && Patterns.EMAIL_ADDRESS.matcher(mLogin.getText()).matches();
+        //И также добавим корректность ввода самого email. Для этого используем Patterns.
+        // EMAIL_ADDRESS.matcher и передадим в него тот же текст из логина. И вызовем метод matches.
+    }
+
+    private boolean isPasswordValid() {
+        return TextUtils.isEmpty(mPassword.getText());
+    }
+
+    private void showMessage(@StringRes int string) {
+        Toast.makeText(this, string, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) { // Bundle обертка над массивом данных
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ac_auth);
+
+        mLogin = findViewById(R.id.etLogin);
+        mPassword = findViewById(R.id.etPassword);
+        mEnter = findViewById(R.id.buttonEnter);
+        mRegister = findViewById(R.id.buttonRegister);
+
+        mEnter.setOnClickListener(mOnEnterClickListener);
+        mRegister.setOnClickListener(mOnRegisterClickListener);
+    }
+}
