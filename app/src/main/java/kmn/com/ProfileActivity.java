a80a35198b17c7a0373ea91944096dd484cd2545
Mederbek <kanatbekov.meder.00@gmail.com>
2021-01-27 00:59:09 +0600

    created menu Logout

--- app/src/main/java/kmn/com/ProfileActivity.java
+++ app/src/main/java/kmn/com/ProfileActivity.java
@@ -1,10 +1,14 @@
 package kmn.com;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatImageView;
@@ -39,5 +43,25 @@ public class ProfileActivity extends AppCompatActivity {
         mPassword.setText(user.getmPassword());
 
         mPhoto.setOnClickListener(mOnPhotoClickListener);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.profile_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.actionLogout :
+                startActivity(new Intent(this, AuthActivity.class));
+                finish();
+                break;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
